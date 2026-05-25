adb524b9a5b988cffa568a08c3e1bbebc3a24c7c
Ajay Kumar <ajaychimata205@gmail.com>
2026-05-25 15:01:42 +0530

    Updates

--- assets/AdminNotifications-DUSx2nri.js
+++ assets/AdminNotifications-DUSx2nri.js
@@ -1,4 +1,4 @@
-import{c as r,r as a,j as e,Z as i,s as n}from"./index-D66Ur4HB.js";import{C as d,c as o}from"./card-J_Xb6LbJ.js";/**
+import{c as r,r as a,j as e,Z as i,s as n}from"./index-DXvt1v1Z.js";import{C as d,c as o}from"./card-DTJoENab.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
