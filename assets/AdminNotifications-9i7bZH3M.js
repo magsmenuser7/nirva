75fbb5abcccfc923e6233aae4ac7c4ff32b08853
magsmenuser7 <magsmenuser7@gmail.com>
2026-07-07 19:20:35 +0530

    Updates

--- assets/AdminNotifications-9i7bZH3M.js
+++ assets/AdminNotifications-9i7bZH3M.js
@@ -1,4 +1,4 @@
-import{c as r,r as a,j as e,$ as i,s as n}from"./index-BhmaxfhF.js";import{C as d,c as o}from"./card-qwNC8PdY.js";/**
+import{c as r,r as a,j as e,$ as i,s as n}from"./index-RDP1Hd6L.js";import{C as d,c as o}from"./card-BBEkhmQY.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
